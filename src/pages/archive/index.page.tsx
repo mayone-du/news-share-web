63ed9205c4e30a0995b823f52f9081ad5878f995
mayone-du <cocomayo1201@icloud.com>
2022-02-27 08:29:04 +0900

    add: archive prev-next day button

--- src/pages/archive/index.page.tsx
+++ src/pages/archive/index.page.tsx
@@ -11,6 +11,7 @@ import { hyphenFormat } from "src/utils";
 
 const ArchiveIndexPage: CustomNextPage = () => {
   const yesterday = dayjs().subtract(1, "day").format(hyphenFormat);
+  const tomorrow = dayjs().add(1, "day").format(hyphenFormat);
   const [searchDate, setSearchDate] = useState(yesterday);
   const newsListQueryResult = useNewsListQuery({
     variables: { input: { sharedAt: searchDate } },
@@ -19,9 +20,30 @@ const ArchiveIndexPage: CustomNextPage = () => {
     if (!e.target.value) return;
     setSearchDate(dayjs(e.target.value).format(hyphenFormat));
   };
+  const handleChangeSearchYesterDay = () => setSearchDate(yesterday);
+  const handleChangeSearchTomorrow = () => setSearchDate(tomorrow);
+
   return (
     <>
       <NextSeo title={ROUTE_LABELS.ARCHIVE} />
+      <div className="flex items-center justify-between mb-4">
+        <button
+          className={`block border rounded px-4 py-2 shadow hover:bg-gray-50 transition-colors ${
+            searchDate === yesterday ? "bg-blue-50" : ""
+          }`}
+          onClick={handleChangeSearchYesterDay}
+        >
+          昨日
+        </button>
+        <button
+          className={`block border rounded px-4 py-2 shadow hover:bg-gray-50 transition-colors ${
+            searchDate === tomorrow ? "bg-blue-50" : ""
+          }`}
+          onClick={handleChangeSearchTomorrow}
+        >
+          明日
+        </button>
+      </div>
       <input
         type="date"
         className="block py-2 px-4 mb-4 w-full rounded border border-gray-500 outline-none"
