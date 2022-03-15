b82f1fcc8d9637681393160fc2bbaf41d0cd5d9f
mayone-du <cocomayo1201@icloud.com>
2022-03-15 23:13:06 +0900

    add: icon

--- src/layouts/SidebarLeft.tsx
+++ src/layouts/SidebarLeft.tsx
@@ -4,6 +4,7 @@ import { SIDEBAR_LEFT_MENUS } from "src/constants/menus";
 import { useRouter } from "next/router";
 import { useCreateNewsModal } from "src/hooks/useCreateNewsModal";
 import { PostSlackButton } from "src/components/common/PostSlackButton";
+import { IoMdAddCircleOutline } from "react-icons/io";
 
 export const SidebarLeft: VFC = () => {
   const { asPath } = useRouter();
@@ -32,9 +33,10 @@ export const SidebarLeft: VFC = () => {
           })}
         </ul>
         <button
-          className="block py-2 mt-4 px-4 rounded border shadow-sm transition-all hover:bg-gray-50 hover:shadow dark:bg-zinc-700 dark:hover:bg-zinc-600"
+          className="flex items-center gap-2 py-2 mt-4 px-4 rounded border shadow-sm transition-all hover:bg-gray-50 hover:shadow dark:bg-zinc-700 dark:hover:bg-zinc-600 text-gray-900"
           onClick={handleOpenCreateNewsModal}
         >
+          <IoMdAddCircleOutline className="block w-6 h-6 text-gray-500" />
           ニュース・記事を投稿する
         </button>
         <PostSlackButton />
