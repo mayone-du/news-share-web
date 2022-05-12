656c8a178d60cdf9e226d97c3c3484596db39389
mayone-du <cocomayo1201@icloud.com>
2022-05-12 23:39:39 +0900

    wip: screen record

--- src/layouts/SidebarRight/Recorder.tsx
+++ src/layouts/SidebarRight/Recorder.tsx
@@ -4,7 +4,7 @@ import { useEffect, VFC } from "react";
 // WIP
 
 const handleStartRecord = () => {
-  navigator.mediaDevices.getDisplayMedia({ audio: false, video: true }).then((stream) => {
+  navigator.mediaDevices.getDisplayMedia({ audio: true, video: true }).then((stream) => {
     const tracks = [...stream.getTracks()];
     const mediaStream = new MediaStream(tracks);
     const rec = new MediaRecorder(mediaStream, { mimeType: "video/webm; codecs=vp9" });
@@ -13,17 +13,16 @@ const handleStartRecord = () => {
     rec.ondataavailable = (ev) => chunks.push(ev.data);
     rec.start();
     rec.onstop = () => {
-      console.log("onstop");
       const webm = new Blob(chunks, { type: "video/webm" });
-      const url = window.URL.createObjectURL(webm);
+      const url = URL.createObjectURL(webm);
       const a = document.createElement("a");
       a.setAttribute("href", url);
       a.setAttribute("download", "rec.webm");
       a.click();
+      URL.revokeObjectURL(url);
     };
 
     setTimeout(() => {
-      console.log("stop");
       rec.stop();
     }, 10000);
   });
@@ -32,7 +31,7 @@ const handleStartRecord = () => {
 export const Recorder: VFC = () => {
   return (
     <div>
-      <Button onClick={handleStartRecord}>Start Recording!</Button>
+      <Button onClick={handleStartRecord}>ニュースシェアを開始する</Button>
     </div>
   );
 };
--- src/layouts/SidebarRight/index.tsx
+++ src/layouts/SidebarRight/index.tsx
@@ -2,8 +2,9 @@ import { Box } from "@mantine/core";
 import type { VFC } from "react";
 import { DateTimeCard } from "src/layouts/SidebarRight/DateTimeCard";
 import { SearchFormCard } from "src/layouts/SidebarRight/SearchFormCard";
+import { Recorder } from "src/layouts/SidebarRight/Recorder";
 
-const SidebarRightContents = [DateTimeCard, SearchFormCard];
+const SidebarRightContents = [Recorder, DateTimeCard, SearchFormCard];
 
 export const SidebarRight: VFC = () => {
   return (
