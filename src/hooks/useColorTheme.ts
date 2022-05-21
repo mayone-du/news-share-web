06a9193ea557afd23f7a12af33f87921c8e0ee3b
mayone-du <cocomayo1201@icloud.com>
2022-05-21 17:06:42 +0900

    wip

--- src/hooks/useColorTheme.ts
+++ src/hooks/useColorTheme.ts
@@ -1,6 +1,5 @@
 import type { ColorScheme } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
-import { useRouter } from "next/router";
 
 // wip
 export const useColorTheme = () => {
@@ -9,8 +8,14 @@ export const useColorTheme = () => {
     defaultValue: "light",
     getInitialValueInEffect: true,
   });
-  const { push } = useRouter();
 
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  // TODO: html要素のclassNameとかを更新してtailwindに反応させる
+
+  return {
+    colorScheme,
+    toggleColorScheme,
+  };
 };
